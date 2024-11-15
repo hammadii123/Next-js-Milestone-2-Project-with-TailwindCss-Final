13e8661f7f8d382da269436b164742084f54f50f
HammadMustafa <hammadworks123@gmail.com>
2024-11-15 18:50:26 +0500

    edited contactForm

--- pages/contact.js
+++ pages/contact.js
@@ -19,16 +19,15 @@ const contact = () => {
             marginWidth={0}
             scrolling="no"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d924237.7104053638!2d66.49592024324728!3d25.192983564783333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e06651d4bbf%3A0x9cf92f44555a0c23!2sKarachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e0!3m2!1sen!2s!4v1731182729613!5m2!1sen!2s"
-            style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
+            style={{ filter: " contrast(1.2) opacity(0.4)" }}
           />
           <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
+            <div className="lg:w-1/4 px-6">
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
                 ADDRESS
               </h2>
-              <p className="mt-1">
-                Photo booth tattooed prism, portland taiyaki hoodie neutra
-                typewriter
+              <p className="mt-1 text-gray-900">
+                Karachi 
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -36,21 +35,18 @@ const contact = () => {
                 EMAIL
               </h2>
               <a className="text-indigo-500 leading-relaxed">
-                example@email.com
+                hammadworks123@email.com
               </a>
-              <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
-                PHONE
-              </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              
             </div>
           </div>
         </div>
-        <div className="lg:w-1/3 md:w-1/2  bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+        <div className="lg:w-1/3 md:w-1/2  flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white text-lg mb-1 font-medium title-font">
-            Feedback
+            Contact me !
           </h2>
           <p className="leading-relaxed mb-5 text-white">
-            Post-ironic portland shabby chic echo park, banjo fashion axe
+            Lets fulfill your dreams in the world of technology
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-white">
@@ -88,13 +84,10 @@ const contact = () => {
               defaultValue={""}
             />
           </div>
-          <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Submit
+          <button className="text-white bg-purple-900 border-0 py-2 px-6 focus:outline-none hover:bg-green-800 rounded text-lg">
+            Send a Message
           </button>
-          <p className="text-xs text-gray-500 mt-3">
-            Chicharrones blog helvetica normcore iceland tousled brook viral
-            artisan.
-          </p>
+         
         </div>
       </div>
     </section>
